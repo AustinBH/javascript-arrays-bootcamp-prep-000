e47296e510e2d91731ecb3b6e0e218b3a3cf7d95
Austin Harlow <harlowaustin2@gmail.com>
2019-04-27 22:16:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,12 +19,13 @@ array.push(element);
 return array
 }
 
-function accessElementInArray() {
-
+function accessElementInArray(array, index) {
+  return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray() {
-
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+array.shift();
+return array
 }
 
 function removeelementFromBeginningOfArray() {
