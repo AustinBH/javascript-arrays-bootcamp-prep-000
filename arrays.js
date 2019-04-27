465ee524ba321680dec7670c50e6e255df45b55c
Austin Harlow <harlowaustin2@gmail.com>
2019-04-27 22:04:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray() {
 
 
-  ["Baby Ruth", ...chocolateBars]
+  chocolateBars = ["Baby Ruth", ...chocolateBars]
 
 }
 
