6a0549933850fdc00105112784ab893f2c938477
Austin Harlow <harlowaustin2@gmail.com>
2019-04-27 21:58:49 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray() {
+
+
+  ["Baby Ruth", ...chocolateBars]
+
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+  chocolateBars.unshift("Baby Ruth")
+
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
+
+function accessElementInArray() {
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+
+}
