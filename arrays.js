8329739301f15118bc24d6a3dfa871f37a352298
Austin Harlow <harlowaustin2@gmail.com>
2019-04-27 22:46:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,9 +34,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1);
+  array.pop()
   return array
-}
 
 function removeElementFromEndOfArray() {
 
